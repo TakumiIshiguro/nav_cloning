e00be6f0f525ef0fc022a078f90201aac58b923e
TakumiIshiguro <aquarius1496@gmail.com>
2024-11-19 23:52:19 +0900

    update

--- scripts/analysis/my_result.py
+++ scripts/analysis/my_result.py
@@ -10,7 +10,7 @@ import os
 import roslib
 lists = []
 
-experiment = 'test'
+experiment = 'route_8/2'
 
 for filename in sorted(glob.glob('/home/takumi/catkin_ws/src/nav_cloning/data/result_with_dir_selected_training/' + experiment + '/*/training.csv'), key=lambda f: os.stat(f).st_mtime, reverse=True):
 # for filename in sorted(glob.glob('/home/yuzuki/ws/master_model_evaluation_ws/src/nav_cloning/data/result_use_dl_output/*/training_all.csv'), key=lambda f: os.stat(f).st_mtime, reverse=True):
@@ -60,12 +60,12 @@ def draw_training_pos_tsudanuma():
     index = 0
     file_number = 1
     while index < 10000:
-        image = Image.open(roslib.packages.get_pkg_dir('nav_cloning')+'/maps/cit_3f_map.pgm').convert("L")
+        image = Image.open(roslib.packages.get_pkg_dir('nav_cloning')+'/maps/real_tsudanuma2-3_v2.pgm').convert("L")
         image = image.crop((1762, 1253, 2059, 2252))
         arr = np.asarray(image)
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.imshow(arr, cmap='gray', extent=[-12.5,3,-12.5,37.2])
+        ax.imshow(arr, cmap='gray', extent=[-12.25,3,-12.5,37.2])
         vel = 0.2
         arrow_dict = dict(arrowstyle = "->", color = "black")
         count = 0
@@ -76,7 +76,7 @@ def draw_training_pos_tsudanuma():
                     str_step, mode, nazo, angle_error, distance, str_x, str_y, str_the, cmd_dir = row
                     if mode == "test":
                         x, y = float(str_x), float(str_y)
-                        patch = Circle(xy=(x, y), radius=0.08, facecolor="red") 
+                        patch = Circle(xy=(-x-5, -y+7.7), radius=0.08, facecolor="red") 
                         ax.add_patch(patch)
             else:
                         pass
@@ -94,9 +94,48 @@ def draw_training_pos_tsudanuma():
         index += 1
         file_number += 1
 
+# def draw_training_pos_oldtsudanuma():
+#     index = 0
+#     file_number = 1
+#     while index < 10000:
+#         image = Image.open(roslib.packages.get_pkg_dir('nav_cloning')+'/maps/cit_3f_map.pgm').convert("L")
+#         image = image.crop((1762, 1253, 2059, 2252))
+#         arr = np.asarray(image)
+#         fig = plt.figure()
+#         ax = fig.add_subplot(111)
+#         ax.imshow(arr, cmap='gray', extent=[-12.25,3,-12.5,37.2])
+#         vel = 0.2
+#         arrow_dict = dict(arrowstyle = "->", color = "black")
+#         count = 0
+
+#         print(file_number)
+#         with open(lists[index]) as f:
+#             for row in csv.reader(f):
+#                     str_step, mode, nazo, angle_error, distance, str_x, str_y, str_the, cmd_dir = row
+#                     if mode == "test":
+#                         x, y = float(str_x), float(str_y)
+#                         patch = Circle(xy=(x, y), radius=0.08, facecolor="red") 
+#                         ax.add_patch(patch)
+#             else:
+#                         pass
+        
+#         ax.set_xlim([-13, 3])
+#         ax.set_ylim([-10, 35]) 
+#         plt.show(block=False)
+#         pictures_dir = '/home/takumi/catkin_ws/src/nav_cloning/data/pictures/' + experiment
+#         if not os.path.exists(pictures_dir):
+#             os.makedirs(pictures_dir)
+#         plt.savefig(pictures_dir + "/" + str(file_number) + ".png")
+#         plt.pause(1)
+#         plt.close()
+
+#         index += 1
+#         file_number += 1
+
 if __name__ == '__main__':
     # draw_training_pos_willow()
     draw_training_pos_tsudanuma()
+    # draw_training_pos_oldtsudanuma():
 
 
 
